94acad2d529ea02564e86f74f6865841d6ec4ab7
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-05-09 17:54:58 +0400

    prepared the project for work

---
+++ CoinGesckoAPI/Models/Coin.swift
@@ -0,0 +1,8 @@
+//
+//  Coin.swift
+//  CoinGesckoAPI
+//
+//  Created by Iaroslav Beldin on 09.05.2023.
+//
+
+import Foundation
---
+++ CoinGesckoAPI/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  CoinGesckoAPI
+//
+//  Created by Iaroslav Beldin on 09.05.2023.
+//
+
+import Foundation
